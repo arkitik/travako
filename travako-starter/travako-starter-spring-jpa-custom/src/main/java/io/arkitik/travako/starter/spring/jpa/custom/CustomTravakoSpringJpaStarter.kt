6f6f4c95502b3b41a54882badd6eb20fa606467f
Ibrahim AlTamimi <ibm.iloom@gmail.com>
2022-02-05 17:14:40 +0200

    fix: Fix multiple database instances configurations.

--- travako-starter/travako-starter-spring-jpa-custom/src/main/java/io/arkitik/travako/starter/spring/jpa/custom/CustomTravakoSpringJpaStarter.kt
+++ travako-starter/travako-starter-spring-jpa-custom/src/main/java/io/arkitik/travako/starter/spring/jpa/custom/CustomTravakoSpringJpaStarter.kt
@@ -1,46 +1,26 @@
 package io.arkitik.travako.starter.spring.jpa.custom
 
 import com.zaxxer.hikari.HikariDataSource
-import io.arkitik.travako.adapter.job.event.repository.TravakoJobEventRepository
-import io.arkitik.travako.adapter.job.event.repository.TravakoRunnerJobEventStateRepository
-import io.arkitik.travako.adapter.job.repository.TravakoJobInstanceRepository
 import io.arkitik.travako.adapter.leader.LeaderPortContext
-import io.arkitik.travako.adapter.leader.repository.TravakoLeaderRepository
-import io.arkitik.travako.adapter.runner.repository.TravakoSchedulerRunnerRepository
-import io.arkitik.travako.adapter.server.repository.TravakoServerRepository
-import io.arkitik.travako.entity.job.TravakoJobInstance
-import io.arkitik.travako.entity.job.event.TravakoJobEvent
-import io.arkitik.travako.entity.job.event.TravakoRunnerJobEventState
-import io.arkitik.travako.entity.leader.TravakoLeader
-import io.arkitik.travako.entity.runner.TravakoSchedulerRunner
-import io.arkitik.travako.entity.server.TravakoServer
 import io.arkitik.travako.port.job.JobInstancePortContext
 import io.arkitik.travako.port.job.event.JobEventPortContext
 import io.arkitik.travako.port.runner.RunnerPortContext
 import io.arkitik.travako.port.server.ServerPortContext
 import io.arkitik.travako.port.shared.SharedPortContext
-import org.springframework.beans.factory.ObjectProvider
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
-import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryBuilderCustomizer
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
-import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.orm.jpa.JpaTransactionManager
-import org.springframework.orm.jpa.JpaVendorAdapter
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
-import org.springframework.orm.jpa.persistenceunit.PersistenceUnitManager
-import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 import org.springframework.transaction.PlatformTransactionManager
-import javax.persistence.EntityManager
 import javax.sql.DataSource
 
 /**
@@ -49,6 +29,11 @@ import javax.sql.DataSource
  * Project *travako* [arkitik.io](https://arkitik.io)
  */
 @Configuration
+@EnableJpaRepositories(
+    basePackages = ["io.arkitik.travako.adapter"],
+    entityManagerFactoryRef = "travakoEntityManagerFactory",
+    transactionManagerRef = "travakoTransactionManager"
+)
 @Import(value = [
     SharedPortContext::class,
     RunnerPortContext::class,
@@ -59,24 +44,8 @@ import javax.sql.DataSource
 ])
 class CustomTravakoSpringJpaStarter {
     @Bean
-    @Primary
     @ConfigurationProperties("arkitik.travako.database.datasource")
     fun travakoDataSourceProperties() = DataSourceProperties()
-
-    @Bean
-    @Primary
-    @ConfigurationProperties("arkitik.travako.database.init")
-    fun travakoSqlInitializationProperties() = SqlInitializationProperties()
-
-    @Bean
-    @Primary
-    @ConfigurationProperties("arkitik.travako.database.hibernate")
-    fun travakoHibernateProperties() = HibernateProperties()
-
-    @Bean
-    @Primary
-    @ConfigurationProperties("arkitik.travako.database.jpa")
-    fun travakoJpaProperties() = JpaProperties()
 
     @Bean
     @Primary
@@ -87,61 +56,24 @@ class CustomTravakoSpringJpaStarter {
         .build()
 
     @Bean
-    @ConditionalOnMissingBean
-    fun travakoJpaVendorAdapter(
-        travakoJpaProperties: JpaProperties,
-    ): JpaVendorAdapter {
-        val adapter: AbstractJpaVendorAdapter = HibernateJpaVendorAdapter()
-        adapter.setShowSql(travakoJpaProperties.isShowSql)
-        if (travakoJpaProperties.database != null) {
-            adapter.setDatabase(travakoJpaProperties.database)
-        }
-        if (travakoJpaProperties.databasePlatform != null) {
-            adapter.setDatabasePlatform(travakoJpaProperties.databasePlatform)
-        }
-        adapter.setGenerateDdl(travakoJpaProperties.isGenerateDdl)
-        return adapter
-    }
-
-    @Bean
-    fun travakoEntityManagerFactoryBuilder(
-        travakoJpaVendorAdapter: JpaVendorAdapter,
-        travakoJpaProperties: JpaProperties,
-        persistenceUnitManager: ObjectProvider<PersistenceUnitManager?>,
-        customizers: ObjectProvider<EntityManagerFactoryBuilderCustomizer>,
-    ): EntityManagerFactoryBuilder? {
-        val builder = EntityManagerFactoryBuilder(travakoJpaVendorAdapter,
-            travakoJpaProperties.properties, persistenceUnitManager.ifAvailable)
-        customizers.orderedStream().forEach { customizer: EntityManagerFactoryBuilderCustomizer ->
-            customizer.customize(builder)
-        }
-        return builder
-    }
-
-    @Bean
     @Primary
     fun travakoEntityManagerFactory(
-        travakoEntityManagerFactoryBuilder: EntityManagerFactoryBuilder,
+        builder: EntityManagerFactoryBuilder,
         travakoAppDataSource: DataSource,
-        travakoJpaProperties: JpaProperties,
-        travakoHibernateProperties: HibernateProperties,
+        jpaProperties: JpaProperties,
+        hibernateProperties: HibernateProperties,
     ): LocalContainerEntityManagerFactoryBean =
-        travakoEntityManagerFactoryBuilder.dataSource(travakoAppDataSource)
+        builder.dataSource(travakoAppDataSource)
             .properties(
-                travakoHibernateProperties.determineHibernateProperties(
-                    travakoJpaProperties.properties,
+                hibernateProperties.determineHibernateProperties(
+                    jpaProperties.properties,
                     HibernateSettings().ddlAuto {
-                        travakoHibernateProperties.ddlAuto
+                        hibernateProperties.ddlAuto
                     }
                 )
             )
-            .packages(
-                "io.arkitik.travako.entity.runner",
-                "io.arkitik.travako.entity.job",
-                "io.arkitik.travako.entity.job.event",
-                "io.arkitik.travako.entity.server",
-                "io.arkitik.travako.entity.leader",
-            ).build()
+            .packages("io.arkitik.travako.entity")
+            .build()
 
     @Bean
     @Primary
@@ -149,34 +81,4 @@ class CustomTravakoSpringJpaStarter {
         travakoTransactionManager: LocalContainerEntityManagerFactoryBean,
     ): PlatformTransactionManager = JpaTransactionManager(travakoTransactionManager.getObject()!!)
 
-    @Bean
-    fun travakoLeaderRepository(
-        travakoEntityManagerFactory: EntityManager,
-    ) = createRepository<TravakoLeader, String, TravakoLeaderRepository>(travakoEntityManagerFactory)
-
-    @Bean
-    fun travakoServerRepository(
-        travakoEntityManagerFactory: EntityManager,
-    ) = createRepository<TravakoServer, String, TravakoServerRepository>(travakoEntityManagerFactory)
-
-    @Bean
-    fun travakoJobInstanceRepository(
-        travakoEntityManagerFactory: EntityManager,
-    ) = createRepository<TravakoJobInstance, String, TravakoJobInstanceRepository>(travakoEntityManagerFactory)
-
-    @Bean
-    fun travakoSchedulerRunnerRepository(
-        travakoEntityManagerFactory: EntityManager,
-    ) = createRepository<TravakoSchedulerRunner, String, TravakoSchedulerRunnerRepository>(travakoEntityManagerFactory)
-
-    @Bean
-    fun travakoRunnerJobEventStateRepository(
-        travakoEntityManagerFactory: EntityManager,
-    ) = createRepository<TravakoRunnerJobEventState, String, TravakoRunnerJobEventStateRepository>(
-        travakoEntityManagerFactory)
-
-    @Bean
-    fun travakoJobEventRepository(
-        travakoEntityManagerFactory: EntityManager,
-    ) = createRepository<TravakoJobEvent, String, TravakoJobEventRepository>(travakoEntityManagerFactory)
 }
