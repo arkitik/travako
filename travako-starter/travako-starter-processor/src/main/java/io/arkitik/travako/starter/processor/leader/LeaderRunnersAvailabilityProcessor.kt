abe78fb81344bbb4c4fb67bb278e613368d5a0e8
Ibrahim AlTamimi <ibm.iloom@gmail.com>
2022-03-31 12:01:42 +0300

    fix: Fix leader-recovering processor while fetching the runners from the database and the runners shared their keys with different hosts.

--- travako-starter/travako-starter-processor/src/main/java/io/arkitik/travako/starter/processor/leader/LeaderRunnersAvailabilityProcessor.kt
+++ travako-starter/travako-starter-processor/src/main/java/io/arkitik/travako/starter/processor/leader/LeaderRunnersAvailabilityProcessor.kt
@@ -48,7 +48,7 @@ class LeaderRunnersAvailabilityProcessor(
                     runners.filter {
                         it.isRunning || it.isLeader
                     }.filter {
-                        travakoConfig.isSelf(it.runnerKey, it.runnerHost)
+                        !travakoConfig.isSelf(it.runnerKey, it.runnerHost)
                     }.filter {
                         it.heartbeatLessThanExpectedTime(lastHeartbeatSeconds)
                     }.forEach {
